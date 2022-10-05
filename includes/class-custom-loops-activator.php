1cba8181d6d1e79130b2b5871f31db3d55cdf47e
DanieGalue <daniela.galue10@gmail.com>
2022-10-05 13:56:24 -0400

    Add files via upload

---
+++ includes/class-custom-loops-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/DanieGalue/
+ * @since      1.0.0
+ *
+ * @package    Custom_Loops
+ * @subpackage Custom_Loops/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Custom_Loops
+ * @subpackage Custom_Loops/includes
+ * @author     DG <daniela.galue10@gmail.com>
+ */
+class Custom_Loops_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-custom-loops-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/DanieGalue/
+ * @since      1.0.0
+ *
+ * @package    Custom_Loops
+ * @subpackage Custom_Loops/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Custom_Loops
+ * @subpackage Custom_Loops/includes
+ * @author     DG <daniela.galue10@gmail.com>
+ */
+class Custom_Loops_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
